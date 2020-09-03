5ab804c88b7c52e8c7af15670fe8d519d6914f90
John Garry <johngarry@MacBook-Pro.local>
2020-09-03 20:49:28 +0100

    supertiled

---
+++ Sniper Game/Assets/SuperTiled2Unity/Import/basicTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="basicTileset" tilewidth="24" tileheight="24" tilecount="4" columns="2">
+ <image source="Combo.png" width="49" height="49"/>
+</tileset>
